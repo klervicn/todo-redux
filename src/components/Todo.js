2519174521f3c5a2eaeaaa467025dcc2b2c97e18
Klervi Corbel <corbel.klervi@gmail.com>
2017-12-18 01:51:22 +0100

    Remove check button
    Click to check item

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 
 const Todo = ({ onTodoClick, onDeleteClick, isCompleted, text }) => (
   <li
+    onClick={onTodoClick}
     style={{
       textDecoration: isCompleted ? 'line-through' : 'none'
     }}
   >
     {text}
-    <button onClick={onTodoClick}>Check</button>
     <button onClick={onDeleteClick}>Delete</button>
   </li>
 );
