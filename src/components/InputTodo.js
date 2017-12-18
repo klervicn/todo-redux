183047e2da8ee6e90be2c1a856fae6c8365f9924
Klervi Corbel <corbel.klervi@gmail.com>
2017-12-18 01:47:16 +0100

    Create input

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -2,12 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class InputTodo extends React.PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: ''
+    };
+    this.updateInputValue = this.updateInputValue.bind(this);
+    this.addATodo = this.addATodo.bind(this);
+  }
+
+  updateInputValue(evt) {
+    //update state when text is typed
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+
+  addATodo(evt) {
+    evt.preventDefault(); // No refresh
+    this.props.onClick(this.state.inputValue); // Not really certain that's the good way to do this..
+    this.setState({
+      inputValue: ''
+    });
+  }
   render() {
     return (
       <div>
-        <form>
-          <input type="text" />
-          <button>Add</button>
+        <form onSubmit={this.addATodo}>
+          <input
+            type="text"
+            onChange={this.updateInputValue}
+            value={this.state.inputValue}
+          />
+          <button type="submit">Add</button>
         </form>
       </div>
     );
